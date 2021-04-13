c99eab765633457f36ba11f293411d7a915d7f4b
손영산 <54147313+zeromountain@users.noreply.github.com>
2021-04-14 01:47:53 +0900

    생명주기 함수  componentDidMount() 사용하기

--- src/components/Lifecycle.js
+++ src/components/Lifecycle.js
@@ -3,13 +3,19 @@ import React, { Component } from 'react';
 class Lifecycle extends Component {
   static getDerivedStateFromProps(props, state) {
     console.log('2. getDerivedStateFromProps Call: ', props.prop_value);
-    return {};
+    return {tmp_state: props.prop_value};
   }
   constructor(props) {
     super(props) 
     this.state = {};
     console.log('1. constructor Call');
   }
+
+  componentDidMount() {
+    console.log('4. componentDidMount Call');
+    console.log('5. tmp_state:', this.state.tmp_state);
+  }
+
   render () {
     console.log('3. render Call');
     return(
