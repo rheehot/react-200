bdf677c6af567d91048c3b3bc79e04aafd5a15ea
손영산 <54147313+zeromountain@users.noreply.github.com>
2021-04-14 02:23:22 +0900

    전개 연산자 사용하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import SpreadOperator from './components/SpreadOperator';
 import Variable from './components/Variable';
 
 
@@ -9,7 +10,7 @@ function App() {
     <div>
       <h1>Start React 200!</h1>
       <p>HTML 적용하기</p>
-      <Variable />
+      <SpreadOperator />
     </div>
   );
 }
