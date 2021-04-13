8e4d466a6b891dee8ccfd33d49f4cdb8e9d02954
손영산 <54147313+zeromountain@users.noreply.github.com>
2021-04-14 01:34:42 +0900

    Component 사용하기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import ImportComponent from './components/ImportComponent';
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>HTML 적용하기</p>
+      <ImportComponent></ImportComponent>
     </div>
   );
 }
---
+++ src/components/ImportComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ImportComponent extends Component {
+  render () {
+    return (
+      <h2>[THIS IS IMPORTED COMPONENT]</h2>
+    );
+  }
+}
+
+export default ImportComponent;
