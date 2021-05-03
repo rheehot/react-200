84ed57fce5d0ae9e6825bc93716602094643c27d
손영산 <54147313+zeromountain@users.noreply.github.com>
2021-05-03 17:17:07 +0900

    화살표 함수 사용하기

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import './App.css';
-import ClassProto from './components/ClassProto';
-import SpreadOperator from './components/SpreadOperator';
-import Variable from './components/Variable';
+import ArrowFunction from './components/ArrowFunction';
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>HTML 적용하기</p>
-      <ClassProto/>
+      <ArrowFunction />
     </div>
   );
 }
