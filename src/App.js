5cfacccfad16deb088bc37f8623cdd48e9fdd142
손영산 <54147313+zeromountain@users.noreply.github.com>
2021-05-03 17:24:10 +0900

    forEach() 함수 사용하기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import ArrowFunction from './components/ArrowFunction';
+import ForEach from './components/ForEach';
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>HTML 적용하기</p>
-      <ArrowFunction />
+      <ForEach />
     </div>
   );
 }
